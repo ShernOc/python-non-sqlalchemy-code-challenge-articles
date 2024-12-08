96cac4b69e91f5e81927110914e844f1be046a37
ShernOC <sherlynea1994@hotmail.com>
2024-12-08 23:31:55 +0300

    finish challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -35,18 +35,17 @@ class Article:
     @title.setter 
     def title(self,value):
        
-        if hasattr(self,'_title'):
-            raise AttributeError ("The title is cannot be changed")
-        
-        if not isinstance(value, str):
-            raise TypeError("The value has to be a string type")
-        
-        if  not (5 <= len(value)<= 50):
-            raise ValueError(" The characters have  to be between 5 and 50 characters ")
+        if hasattr(self,"title"):
+            AttributeError ("The title is cannot be changed")
+        else:
+            if isinstance(value, str):
+               if 5 <= len(value)<= 50: 
+                    self._title = value
+               else:
+                   ValueError(" The characters have  to be between 5 and 50 characters ")
+                   
+            else: TypeError("The value has to be a string type")
        
-        self._title = value
-
-  
 class Author:
     all = []
     def __init__(self, name):
@@ -60,36 +59,36 @@ class Author:
     #setter 
     @name.setter 
     def name(self,value):
-        if not isinstance(value,str):
-            raise TypeError("The Value has to be of string type")
+        if hasattr(self, "name"):
+            AttributeError("Name cannot be changed after initialization")
+        else: 
+            if isinstance(value,str):
+                if len(value)>=0:
+                    self._name = value
+                else: 
+                    ValueError("The Name value must be longer than 0 character")
+            else: 
+                TypeError("The Value has to be of string type")
         
-        if not len(value)>=0:
-            raise Exception("The value has to greater than 0 characters long. ")
-        
-        if hasattr(self, '_name'):
-            raise AttributeError("Name cannot be changed after initialization")
-        
-        self._name = value
-            
+ 
     def articles(self):
         return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        unique = [article.magazine for article in self.articles() if isinstance(article.magazine, Magazine)]
-        return unique
+        return list ({article.magazine for article in self.articles()})
+    
     
     def add_article(self, magazine, title):
         #return a new Article object
         return Article(author = self, magazine = magazine, title = title) 
 
     def topic_areas(self):
-        unique_list = self.articles()
-        if not unique_list: 
+        topic_areas = list({magazine.category for magazine in self.magazines()})
+        if topic_areas:
+            return topic_areas
+        else:
             return None
-        category = [article.magazine.category for article in unique_list if isinstance(article.magazine, Magazine)]
-        return category
         
-
 class Magazine:
     all = []
     def __init__(self, name, category):
@@ -103,13 +102,14 @@ class Magazine:
     
     @name.setter
     def name(self,value):
-        if hasattr(self, '_name'):
-            raise AttributeError("'Magazine' object has no attribute '_name'")
-        if not isinstance(value,str):
-            raise AttributeError("The name is not of string type")
-        if not 2<= len(value)<=16:
-            raise Exception("Values has to be of 2,16 characters")
-        self._name = self
+        
+        if isinstance(value,str):
+            if 2<= len(value)<=16:
+                self._name = value
+            else:  
+                ValueError("Values has to be of 2 and 16 characters")
+        else: 
+            TypeError("Name must be a string")
     
     @property
     def category(self):
@@ -117,57 +117,42 @@ class Magazine:
     
     @category.setter
     def category(self,value):
-        if not isinstance(value,str):
-            raise TypeError("The name is not of string type")
-        
-        if not len(value)>=0:
-            raise Exception("Category has to have more that 0 characters")
-        self._category = value
-            
-        
+        if isinstance(value,str):
+            if len(value):
+               self._category = value  
+            else: 
+             TypeError("The name is not of string type")
+        else: 
+            Exception("Category has to have more that 0 characters")
+       
     def articles(self):
         return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        unique_list = [unique.author for unique in self.articles() if isinstance(unique.author,Author)]
+        unique_list = list ({unique.author for unique in self.articles()})
         return unique_list
     
     def article_titles(self):
-        titles_list = [article.title for article in Article.all  if article.magazine == self]
-        if not titles_list:
-            return None  
-        return titles_list
+        titles_list = [article.title for article in self.articles()]
+        if titles_list:
+            return titles_list
+        return None
 
     def contributing_authors(self):
-        #return authors who have writted more that 2 articles. 
-        author_count = {}  #counts the article per author
-        # Count the articles by each author for this magazine
+        authors = {}
+        list_of_authors = []
         for article in self.articles():
-            if isinstance(article.author, Author):
-                if article.author not in author_count:
-                    author_count[article.author] = 0
-                author_count[article.author] += 1
+            if article.author in authors:
+                authors[article.author] += 1
+            else:
+                authors[article.author] = 1  
         
-        #author with more than two 
-        authors_2= [author for author, count in author_count.items() if count > 2
-        ]
-        
-        if authors_2:
-            return authors_2
+        for author in authors:
+            if authors[author] >= 2:
+                list_of_authors.append(author) 
+                  
+        if (list_of_authors):
+            return list_of_authors
         else:
-            return None  
-    
-    @classmethod
-    def top_publisher(cls):
-        if not cls.all:
             return None
     
-        top_magazine = max(cls.all, key = lambda magazine: len(magazine.articles()))
-        
-        if len(top_magazine.articles()) > 0:
-            return top_magazine
-        else:
-            return None  
-  
-       
-        
